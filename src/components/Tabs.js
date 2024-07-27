4d0d3abc6956a14c35859a262ff58cceadfbdaeb
HAMUZA <kiggunduhamuza16@gmail.com>
2024-07-26 23:50:19 -0700

    first

---
+++ src/components/Tabs.js
@@ -0,0 +1,28 @@
+import React,{useState} from "react";
+
+
+const Tabs = ({tabs}) => {
+    const [activeTab, setActiveTab] = useState(0);
+
+    const handleTabClick = (index) => {
+        setActiveTab(index);
+    }
+
+    return (
+       <div>
+         <ul>
+            {tabs.map((tab,index)=>{
+                <li key={index} onClick={()=> handleTabClick(index)}>
+                 {tab.index}
+                </li>
+            })}
+         </ul>
+         <div>
+             {tabs[activeTab].content}
+         </div>
+
+       </div>
+    )
+}
+
+export default Tabs;
